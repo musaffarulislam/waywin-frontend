e1e7581fc2f956aa91e48ddcd06e7061ac854578
musaffarulislam <musaffarulislam@gmail.com>
2023-08-05 14:14:51 +0530

    git CI/CD pipeline 11

---
+++ src/firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt26Mff3owkgy65t1hD1rOJGl9cyGW1nE",
+  authDomain: "waywin-phonenumber.firebaseapp.com",
+  projectId: "waywin-phonenumber",
+  storageBucket: "waywin-phonenumber.appspot.com",
+  messagingSenderId: "348826140094",
+  appId: "1:348826140094:web:07f42644d58a2eee378def"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export {auth}
